4bbdbffbb3ded2e92bf81b48ec31c4ea520f9ded
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-07 11:22:10 +0700

    add all file modifile

--- index.js
+++ index.js
@@ -1,16 +1,98 @@
-let h1 = document.createElement('h1')
-let root = document.getElementById('root')
+// REACT.CREAREELEMENT () => React element
 
-h1.innerText='毎日ITを勉強してる'
-h1.id = 'heading'
-h1.className = 'test class2'
-// h1.style = 'color: green; font-size: 20px' 
-Object.assign(h1.style, {
-    color: 'white',
-    backgroundColor: '#333'
-})
+// React element là thành phần nhỏ nhất của React
 
 
+// DOM
+let h1Dom = document.createElement('h1')
 
-root.appendChild(h1) //appendChild là thêm thằng con vào
+h1Dom.title = 'Hello'
+h1Dom.className = 'Heading'
+h1Dom.innerText = '毎日ITを勉強してるでもむずかしいです'
 
+document.body.appendChild(h1Dom)
+
+// React
+let h1React = React.createElement('h1', {
+    title: 'Hello',
+    className: 'React heading'
+}, 'ITが難しいですので毎日頑張って勉強しているところです' ) 
+
+console.log(h1Dom)
+console.dir(h1React)
+
+
+
+// VD: 2
+//  <ul>
+//     <li>ReactJs</li>
+//     <li>Javascript</li>
+//  </ul>
+
+// DOM
+
+let ulDom = document.createElement('ul')
+ulDom.style = 'color: red'
+ulDom.id = 'react'
+
+let liDOM1 = document.createElement('li')
+liDOM1.innerText = 'ReactJS'
+liDOM1.className = 'find'
+
+let liDOM2 = document.createElement('li')
+liDOM2.innerText = 'Javascript'
+
+ulDom.appendChild(liDOM1)
+ulDom.appendChild(liDOM2)
+
+document.body.appendChild(ulDom)
+
+
+// Reeact
+
+let ulReact = React.createElement(
+    'ul',
+    {
+        id: 'React',
+        className: 'Fine'
+    },
+    React.createElement('li',{className:'find'}, 'ITがむず歌詞いいです'),
+    React.createElement('li', null, '日本語が難しいです')
+)
+
+console.dir(ulReact)
+
+
+// VD3.
+    // <div class="post-item">
+    //     <h2> title="Hoc React tai f8">Hoc React</h2>
+    //     <p>Hoc reacjs tu co ban den nang cao</p>
+    // </div> 
+
+
+// DOM 
+    
+    let divDom = document.createElement('div')
+    divDom.className = "post-item"
+    let h2Dom = document.createElement('h2')
+    h2Dom.innerText = '日本語が難しいですが頑張ってください'
+    h2Dom.title = 'Hoc ReactJs tai f8'
+
+    let pDom = document.createElement('p')
+    pDom.innerText = '毎日専門２時間ぐらい勉強しているところです'
+
+    divDom.appendChild(h2Dom)
+    divDom.appendChild(pDom)
+
+    document.body.appendChild(divDom)
+
+// React
+
+    let divDom2 = React.createElement(
+        'div',
+        {
+            className: 'post-item'
+        },
+        React.createElement('h2', {title: 'Hoc react tai F8'}, 'Study ReactJS'),
+        React,createElement('p', null, 'Learn ReactJS from Zero to Herro' )
+    )
