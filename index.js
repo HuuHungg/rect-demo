f1442c41d473e61309d0fce92ed1b676bc2b3ea2
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-08 21:23:44 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,116 +1,203 @@
-// REACT.CREAREELEMENT () => React element
 
-// React element là thành phần nhỏ nhất của React
-
-
-// DOM
-let h1Dom = document.createElement('h1')
-
-h1Dom.title = 'Hello'
-h1Dom.className = 'Heading'
-h1Dom.innerText = '毎日ITを勉強してるでもむずかしいです'
-
-document.body.appendChild(h1Dom)
-
-// React
-let h1React = React.createElement('h1', {
-    title: 'Hello',
-    className: 'React heading'
-}, 'ITが難しいですので毎日頑張って勉強しているところです' ) 
-
-console.log(h1Dom)
-console.dir(h1React)
+function Header() {
+    return (
+        <div className="header"> New-header </div>
+    )
+}
 
 
-
-// VD: 2
-//  <ul>
-//     <li>ReactJs</li>
-//     <li>Javascript</li>
-//  </ul>
-
-// DOM
-
-let ulDom = document.createElement('ul')
-ulDom.style = 'color: red'
-ulDom.id = 'react'
-
-let liDOM1 = document.createElement('li')
-liDOM1.innerText = 'ReactJS'
-liDOM1.className = 'find'
-
-let liDOM2 = document.createElement('li')
-liDOM2.innerText = 'Javascript'
-
-ulDom.appendChild(liDOM1)
-ulDom.appendChild(liDOM2)
-
-document.body.appendChild(ulDom)
+function Body() {
+    return (
+        <div className='content'> New-content </div>
+    )
+}
 
 
-// Reeact
+class Footer extends React.Component {
+    render() {
+        return (
+              <div className="footer"> New-Footer </div>
+        )
+    }
+}
 
-let ulReact = React.createElement(
-    'ul',
-    {
-        id: 'React',
-        className: 'Fine'
-    },
-    React.createElement('li',{className:'find'}, 'ITがむず歌詞いいです'),
-    React.createElement('li', null, '日本語が難しいです')
+
+let app = (
+
+    <div className = 'wrapper'>
+        <Header />
+        <Body />
+        <Footer />
+    </div>
 )
 
-    // ReactDom @17
-    let root2 = document.getElementById('root2')
-    // ReactDOM.render(ulReact, root2)
 
-    // ReactDOM @18
-        let container2 = ReactDOM.createRoot(root2)
-        container2.render(ulReact)
-
-// VD3.
-    // <div class="post-item">
-    //     <h2> title="Hoc React tai f8">Hoc React</h2>
-    //     <p>Hoc reacjs tu co ban den nang cao</p>
-    // </div> 
+let root = document.getElementById('root')
+let contain = ReactDOM.createRoot(root)
+contain.render(app)
 
 
-// DOM 
-    
-    let divDom = document.createElement('div')
-    divDom.className = "post-item"
-    let h2Dom = document.createElement('h2')
-    h2Dom.innerText = '日本語が難しいですが頑張ってください'
-    h2Dom.title = 'Hoc ReactJs tai f8'
 
-    let pDom = document.createElement('p')
-    pDom.innerText = '毎日専門２時間ぐらい勉強しているところです'
+//
 
-    divDom.appendChild(h2Dom)
-    divDom.appendChild(pDom)
 
-    document.body.appendChild(divDom)
-
-// React
-
-    let divDom2 = React.createElement(
-        'div',
-        {
-            className: 'post-item'
-        },
-        React.createElement('h2', {title: 'Hoc react tai F8'}, 'Study ReactJS'),
-        React.createElement('p', {}, 'Learn ReactJS from Zero to Herro' )
+function PostItem() {
+    return (
+        <div className="post-item">
+                <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZHQzq3399wXUYfIKZYK7l-ROfVVdDPZgwog&usqp=CAU" alt="今朝学校を休み" />
+                <h2 className="post-title">今朝学校を休みました</h2>
+                <p className="post-desc">日本語が難しいですが頑張りましょう</p>
+                <p className="post-public">明日家へ帰るつもりです</p>                
+         </div>
     )
+}
 
-    // Get root element 
-    let container = document.getElementById('root')
-    
-    // React @17
-    // React-DOM -> render UI
-    // ReactDOM.render(divDom2,root) 
 
-    // React @18
-    let root = ReactDOM.createRoot(container)
-    root.render(divDom2)
 
-    
+let app2 = (
+    <div className="post-list">
+        <PostItem />
+        
+
+    </div>
+)
+
+
+let root2 = document.getElementById('root2')
+let contain2 = ReactDOM.createRoot(root2)
+contain2.render(app2)
+
+// Chua bai cach 2
+
+
+function PostItem2({
+    image, 
+    title, 
+    description,
+    publishedAt
+}) {
+    return (
+        <div className="post-item2">
+                <img  
+                    src={image}
+                    alt={title}
+                />
+
+                <h2 className="post-title">{title}</h2>
+                <p className="post-desc">{description}</p>
+                <p className="post-public">{publishedAt}</p>                
+        </div>
+
+    )
+}
+
+
+
+function App() {
+    return(
+        <div id="wrapper">
+            <PostItem2 
+                title= "今朝学校を休みました"
+                description= "明日家へ帰るつもりです"
+                publishedAt = "日本語が難しいですが頑張りましょう"
+                image= "https://vnreview.vn/image/16/81/51/1681516.jpg?t=1499848476274"
+            />
+
+
+        </div>
+
+    )
+}
+
+
+let root3 = document.getElementById('root3')
+
+let contain3 = ReactDOM.createRoot(root3)
+contain3.render(<App/>)
+
+
+
+let courses = [
+    {
+        
+        "description": "Từ cơ bản tới chuyên sâu, thực hành 8 dự án, hàng trăm bài tập, trang hỏi đáp riêng, cấp chứng chỉ sau khóa học và mua một lần học mãi mãi.",
+        "image_url": "https://files.fullstack.edu.vn/f8-prod/courses/15/62f13d2424a47.png",
+        "icon_url": "https://files.fullstack.edu.vn/f8-prod/courses/15/62385d6c63dfa.png",
+        "video_url": "",
+        "landing_page_url": "/landing/htmlcss",
+        "is_pre_order": false,
+        "is_published": true
+      },
+      {
+        
+        "description": "Khóa học JavaScript Pro",
+        "image_url": "https://files.fullstack.edu.vn/f8-prod/courses/19/62f13cb607b4b.png",
+        "icon_url": "https://files.fullstack.edu.vn/f8-prod/courses/19/62f13cb685c81.png",
+        "video_url": "",
+        "landing_page_url": "/landing/javascript-pro",
+        "is_pre_order": false,
+        "is_published": true
+      },
+      {
+        "description": "Khóa học ReactJS Pro",
+        "image_url": "https://files.fullstack.edu.vn/f8-prod/courses/20/62f13dded314e.png",
+        "icon_url": "https://files.fullstack.edu.vn/f8-prod/courses/20/62f13ddf5e7f9.png",
+        "video_url": "",
+        "landing_page_url": "/landing/reactjs-pro",
+        "is_pre_order": false,
+        "is_published": false
+      }
+]
+
+
+
+function PostItem3({
+    image,
+    title,
+    description
+
+}) {
+    return (
+        <div className="post-item3">
+            <img 
+                src={image}
+                alt={title}
+            />
+            <p className="props-des">{description}</p>
+            <p className="props-title">{title}</p>
+
+        </div>
+
+    )
+}
+
+
+
+function Course() {
+    return (
+        <div id="wrapper2">
+            <PostItem3 
+                image = "https://files.fullstack.edu.vn/f8-prod/courses/20/62f13dded314e.png"
+                description = "Từ cơ bản tới chuyên sâu, thực hành 8 dự án, hàng trăm bài tập, trang hỏi đáp riêng, cấp chứng chỉ sau khóa học và mua một lần học mãi mãi."
+                title = "毎日日本語を勉強してる頑張りましょう"
+         />
+            <PostItem3 
+                image = "https://files.fullstack.edu.vn/f8-prod/courses/19/62f13cb607b4b.png"
+                description = "Khóa học JavaScript Pro"
+                title = "ITの専門が難しいですが頑張ってください"
+            />
+            <PostItem3 
+                image = "https://files.fullstack.edu.vn/f8-prod/courses/20/62f13dded314e.png"    
+                description = "Khóa học ReactJS Pro"
+                title = "毎日２時間ぐらいITを勉強するつもりです"
+            />
+
+        </div>
+
+    )
+}
+
+
+let root4 = document.getElementById('root4')
+let contain4 = ReactDOM.createRoot(root4)
+contain4.render(<Course />)
--- react.md
+++ react.md
@@ -1,3 +1,18 @@
-React-Native: Dùng để buit ra thư viện IOSm Android 
-React-Native: Là cầu nối giữa React và Native
+
+- React elements
+    - Sử dụng props giống như với Attribute của thẻ HTML    
+    - 2 props class, for => className, htmlFor
+    - Phải tuân theo quy ước có sẵn
+    
+- React components
+    - Sử dụng props giống như đối số cho Component
+    - Tự do đặt tên props
+        - Đặt theo cameCase
+        - Có thể bao gầm dấu gạch ngang
+- Chú ý 
+    - Prop "key" là prop đặc biệt
+    - Props cơ bản là đối số của Component
+        => Props có thể là bất cứ kiểu dữ liệu gì 
+    - Sử dụng destructuring
+
 
